e795a27769bf3f0fbf382b779fc9154000952863
yuhxu <yhxu@joinhealth.cn>
2021-02-23 10:08:51 +0800

    first commit

---
+++ src/main/java/me/xyh/patchserver/PatchServerApplication.java
@@ -0,0 +1,13 @@
+package me.xyh.patchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatchServerApplication.class, args);
+    }
+
+}
